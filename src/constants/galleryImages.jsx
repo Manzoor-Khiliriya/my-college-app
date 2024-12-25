ee1f20a1f886793eeb863d604a4361d24505499c
Manzoor <akmanchu7@gmail.com>
2024-12-25 15:18:53 +0530

    app updated

--- src/constants/galleryImages.jsx
+++ src/constants/galleryImages.jsx
@@ -1,5 +1,5 @@
 export const galleryImages = [
-  { src: "/images/campusview1.jpg ", alt: "Campus View 1" },
+  { src: "/images/campusview1.jpg", alt: "Campus View 1" },
   { src: "/images/campusview2.jpg", alt: "Campus View 2" },
   { src: "/images/college3.jpg", alt: "Classroom" },
   { src: "/images/college4.jpg", alt: "Sports Field" },
